cb823fb376f32bdcbcd61fbd6ca30d414bf82d6a
Mikhail Yakimchenko <mihanjk@gmail.com>
2017-11-05 19:09:05 +0300

    third task

--- app/src/main/java/com/mihanjk/fintechhomework/Data.kt
+++ app/src/main/java/com/mihanjk/fintechhomework/Data.kt
@@ -1,5 +1,9 @@
 package com.mihanjk.fintechhomework
 
+import java.math.BigDecimal
+
 data class Second(val name: String,
                   val any_map: Map<String, Int>)
+
+data class Third(val money_amount: BigDecimal)
 
--- app/src/main/java/com/mihanjk/fintechhomework/MainActivity.kt
+++ app/src/main/java/com/mihanjk/fintechhomework/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.google.gson.GsonBuilder
-import com.google.gson.TypeAdapter
-import com.google.gson.stream.JsonReader
-import com.google.gson.stream.JsonWriter
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.reflect.Type
+import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,42 +17,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val gson = GsonBuilder()
-                .registerTypeAdapter(Second::class.java, CustomTypeAdapter())
+                .registerTypeAdapter(Third::class.java, BigDecimalDeserializer())
                 .create()
-        val second = gson.fromJson("""{"name":"name","any_map":{"a":"55","b":"85","c":"56"}}""",
-                Second::class.java)
-        textView.text = second.toString()
-        Log.d(javaClass.name, gson.toJson(second))
+        val third = gson.fromJson("""{"money_amount":"2444.88"}""",
+                Third::class.java)
+        textView.text = third.toString()
+        Log.d(javaClass.name, gson.toJson(third))
     }
 }
 
-class CustomTypeAdapter : TypeAdapter<Second>() {
-    override fun read(input: JsonReader): Second {
-        var name = ""
-        val map = HashMap<String, Int>()
-        input.beginObject()
-        while (input.hasNext()) {
-            when (input.nextName()) {
-                "name" -> name = input.nextString()
-                "any_map" -> {
-                    input.beginObject()
-                    while (input.hasNext()) {
-                        map.put(input.nextName(), input.nextInt())
-                    }
-                    input.endObject()
-                }
-            }
-        }
-        input.endObject()
-        return Second(name, map)
-    }
-
-    override fun write(out: JsonWriter, second: Second) {
-        out.beginObject()
-        out.name("name").value(second.name)
-        out.name("any_map").beginObject()
-        second.any_map.forEach { out.name(it.key).value(it.value) }
-        out.endObject()
-        out.endObject()
-    }
+class BigDecimalDeserializer : JsonDeserializer<Third> {
+    override fun deserialize(json: JsonElement, typeOfT: Type?, context: JsonDeserializationContext?): Third =
+            Third(BigDecimal(json.asJsonObject.get("money_amount").asString.replace(',', '.')))
 }
