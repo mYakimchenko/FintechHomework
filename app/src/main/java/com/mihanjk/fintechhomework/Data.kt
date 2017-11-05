0e9531763f62e0ede9f6a5dab299e5cc611d07f9
Mikhail Yakimchenko <mihanjk@gmail.com>
2017-11-05 13:02:32 +0300

    first task

---
+++ app/src/main/java/com/mihanjk/fintechhomework/Data.kt
@@ -0,0 +1,7 @@
+package com.mihanjk.fintechhomework
+
+data class First(val id: Int,
+                 val name: String,
+                 val isFavorite: Boolean,
+                 val dateStamp: Long)
+
--- app/src/main/java/com/mihanjk/fintechhomework/MainActivity.kt
+++ app/src/main/java/com/mihanjk/fintechhomework/MainActivity.kt
@@ -2,11 +2,31 @@ package com.mihanjk.fintechhomework
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.gson.ExclusionStrategy
+import com.google.gson.FieldAttributes
+import com.google.gson.GsonBuilder
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val exclusionStrategy = SomeExclusionStrategy(listOf("isFavorite", "id"), listOf(Long::class.java))
+        val gson = GsonBuilder()
+                .addSerializationExclusionStrategy(exclusionStrategy)
+                /*
+                or we can just use @Expose annotation in data class properties, which needed to
+                serialize, and invoke this method on GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                 */
+                .create()
+        textView.text = gson.toJson(First(21, "name", false, 200003))
     }
+}
+
+class SomeExclusionStrategy(val fieldNames: List<String>, val classes: List<Class<*>>) : ExclusionStrategy {
+    override fun shouldSkipClass(clazz: Class<*>?): Boolean = classes.contains(clazz)
+
+    override fun shouldSkipField(f: FieldAttributes?): Boolean = fieldNames.contains(f?.name)
 }
